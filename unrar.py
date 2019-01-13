6605edc06069cf4214707482c2927795f244fc65
Lcrowther-pivotal <lcrowther@pivotal.io>
2019-01-13 11:56:22 +1000

    TIDY UP

--- unrar.py
+++ unrar.py
@@ -3,22 +3,28 @@ from subprocess import check_call
 from os.path import join
 
 rx = '(.*rar$)|(.*r00$)'
-path = "/Users/xxxxxx/rartest/Sample"
+path = "/Users/XXX/rartest/"
+extensions = "[.mkv,.avi]"
 
-
-def runUnRar(path):
+def runUnRar(path,cmd_part):
     for root, dirs, files in os.walk(path):
-        if not any(f.endswith(".avi") for f in files):
+        if not any(f.endswith(extensions) for f in files):
             found_r = False
             for file in files:
                 pth = join(root, file)
+
                 try:
                      if not found_r and file.endswith((".rar",".r00")):
-                         check_call(["unrar","x", pth, root])
+                         command = cmd_part
+                         command.append(pth)
+                         command.append(root)
+                         check_call(command)
+                         #cmd = ["unrar", "e", "-o-", pth, root]
+                         # #check_call(cmd)
                          found_r = True
                          break
-                except ValueError:
+                except Exception:
                     print ("OOps! That did not work")
 
 
-runUnRar(path)
+runUnRar(path,["unrar", "e", "-o-"])
