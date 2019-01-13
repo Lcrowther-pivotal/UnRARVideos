ce62522378e9edf17cc1f52798d7338f69a10ebd
lcrowther <lcrowther@pivotal.io>
2019-01-13 16:52:28 +1000

    fixed bug within Pyhton 2.3.5 envs

--- unrar.py
+++ unrar.py
@@ -3,7 +3,7 @@ from subprocess import check_call
 from os.path import join
 import os
 
-path = "/Users/XXX/rartest/movies/"
+path = "/Users/lcrowther/rartest/movies/"
 movie_extensions = [".mkv", ".avi"]
 extensions_to_delete = [".rar"]
 #build up full list of ".rxx" files to delete
@@ -14,21 +14,20 @@ for x in xrange(0, 10):
 
 def doUnRAR(path, cmd_part):
     for root, dirs, files in os.walk(path):
-        if not any(f.endswith(tuple(movie_extensions)) for f in files):
-            found_r = False
-            for file in files:
-                pth = join(root, file)
+        found_r = False
+        for file in files:
+            pth = join(root, file)
 
-                try:
-                     if not found_r and file.endswith((".rar",".r00")):
-                         command = cmd_part[:]
-                         command.append(pth)
-                         command.append(root)
-                         check_call(command)
-                         found_r = True
-                         break
-                except Exception as e:
-                    print ("OOps! That did not work: ")
+            try:
+                 if not found_r and file.endswith((".rar",".r00")):
+                     command = cmd_part[:]
+                     command.append(pth)
+                     command.append(root)
+                     check_call(command)
+                     found_r = True
+                     break
+            except Exception as e:
+                print ("OOps! That did not work: ")
 
 def doDelete(path, cmd_part):
     for root,dirs, files in os.walk(path):
